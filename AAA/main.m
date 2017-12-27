33c45d8cf3d1fad43ca38880f27c62b1b625cd28
DuoSanpang <497632551@qq.com>
2017-12-27 18:18:37 +0800

    Initial Commit

---
+++ AAA/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AAA
+//
+//  Created by sanpang on 2017/12/27.
+//  Copyright © 2017年 sanpang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
